244838a717e224225ee3f9da5d5474a319d7687c
tjdux <tjduxxx@gmail.com>
2024-10-06 15:18:14 +0900

    문자열을 활용한 HTML 다루기, 라우터, 페이징

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 // HTML 요소 가져오기
 const container = document.getElementById('root');
 
-// HTML 요소 생성하기 
-const content = document.createElement('div');
-const ul = document.createElement('ul');
-
 // XMLHttpRequest 객체 생성
 const ajax = new XMLHttpRequest();
 
@@ -12,47 +8,97 @@ const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
-// 뉴스 목록 가져오기
-// NEWS_URL에서 GET 요청, false <- 동기적
-ajax.open('GET', NEWS_URL, false);
-// 요청을 서버로 보냄
-ajax.send();
+// 공유되는 자원
+const store = {
+    currentPage: 1,
+}
 
-// 뉴스 데이터 파싱
-// newsFeed <- 배열 
-const newsFeed = JSON.parse(ajax.response);
+
+// 데이터 가져오기 
+function getData(url) {
+    ajax.open('GET', url, false);
+    ajax.send();
+
+    return JSON.parse(ajax.response);
+}
+
+
+// 뉴스 목록 표시
+function newsFeed() {
+    // 뉴스 목록 가져오기
+    const newsFeed = getData(NEWS_URL);
+
+    const newsList = [];
+
+    newsList.push('<ul>');
+
+    for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++){
+        newsList.push(`
+            <li>
+                <a href="#/show/${newsFeed[i].id}">
+                    ${newsFeed[i].title} (${newsFeed[i].comments_count})
+                </a>
+            </li>
+        `);
+    }
+
+    newsList.push('</ul>');
+
+    // 네비게이션 UI
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage-1 : 1}">이전 페이지</a>
+            <a href="#/page/${store.currentPage+1}">다음 페이지</a>
+        </div>
+        `)
+
+    container.innerHTML = newsList.join('');
+}
+
+
+// 뉴스 상세 페이지 
+function newsDetail() {
+    // location: window 객체의 속성, URL에 대한 정보 담고있음
+    const id = location.hash.substring(7);
+
+    // 상세 뉴스 API에 GET 요청
+    // JSON 데이터 파싱 
+    const newsContent = getData(CONTENT_URL.replace("@id", id));
+
+    container.innerHTML = `
+        <h1>${newsContent.title}</h1>
+        <div>
+            <a href="#/page/${store.currentPage}">목록으로</a>
+        </div>
+    `;
+}
+
+
+// 라우터: 뉴스 피드와 뉴스 내용 화면 중계
+function router() {
+    // 해시 값을 기준으로 화면 전환
+    const routePath = location.hash;
+
+    // location.hash에 #만 들어있을 경우 빈 값 반환
+    if (routePath === '') {
+        // 도입 화면
+        newsFeed();
+    } else if (routePath.indexOf('#/page/') >= 0) {
+        // 1️⃣ 이전, 이후 페이지 이동
+        // 2️⃣ 뉴스 내용 -> 뉴스 피드
+        store.currentPage = Number(routePath.substring(7));
+        newsFeed();
+    } else {
+        // 뉴스 피드 -> 뉴스 내용 
+        newsDetail();
+    }
+}
+
+
 
 // 뉴스 항목 클릭 시 상세 내용 표시 (hashChange 이벤트)
 // 해시 값: URL의 # 뒤에 오는 부분으로, 페이지 내 특정 위치나 컨텐츠를 식별하는데 사용
-window.addEventListener('hashchange', function() {
-    // location: window 객체의 속성, URL에 대한 정보 담고있음
-    // #을 제외한 나머지 부분 id 가져옴
-    const id = location.hash.substring(1);
+window.addEventListener('hashchange', router);
 
-    // 상세 뉴스 API에 GET 요청
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-    ajax.send();
-
-    // JSON 데이터 파싱 
-    const newsContent = JSON.parse(ajax.response);
-    const title = document.createElement('h1');
-
-    title.innerHTML = newsContent.title;
-
-    content.appendChild(title);
-});
-
-// 뉴스 목록 표시 
-for (let i = 0; i < 10; i++){
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-
-    a.href = `#${newsFeed[i].id}`;
-    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-
-    li.appendChild(a);
-    ul.appendChild(li);
-}
-
-container.appendChild(ul);
-container.appendChild(content);
+// 첫 진입 화면
+router();
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -121,10 +121,6 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 // HTML 요소 가져오기
 var container = document.getElementById('root');
 
-// HTML 요소 생성하기 
-var content = document.createElement('div');
-var ul = document.createElement('ul');
-
 // XMLHttpRequest 객체 생성
 var ajax = new XMLHttpRequest();
 
@@ -132,45 +128,71 @@ var ajax = new XMLHttpRequest();
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-// 뉴스 목록 가져오기
-// NEWS_URL에서 GET 요청, false <- 동기적
-ajax.open('GET', NEWS_URL, false);
-// 요청을 서버로 보냄
-ajax.send();
+// 공유되는 자원
+var store = {
+  currentPage: 1
+};
 
-// 뉴스 데이터 파싱
-// newsFeed <- 배열 
-var newsFeed = JSON.parse(ajax.response);
+// 데이터 가져오기 
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+}
+
+// 뉴스 목록 표시
+function newsFeed() {
+  // 뉴스 목록 가져오기
+  var newsFeed = getData(NEWS_URL);
+  var newsList = [];
+  newsList.push('<ul>');
+  for (var i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+    newsList.push("\n            <li>\n                <a href=\"#/show/".concat(newsFeed[i].id, "\">\n                    ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n                </a>\n            </li>\n        "));
+  }
+  newsList.push('</ul>');
+
+  // 네비게이션 UI
+  newsList.push("\n        <div>\n            <a href=\"#/page/".concat(store.currentPage > 1 ? store.currentPage - 1 : 1, "\">\uC774\uC804 \uD398\uC774\uC9C0</a>\n            <a href=\"#/page/").concat(store.currentPage + 1, "\">\uB2E4\uC74C \uD398\uC774\uC9C0</a>\n        </div>\n        "));
+  container.innerHTML = newsList.join('');
+}
+
+// 뉴스 상세 페이지 
+function newsDetail() {
+  // location: window 객체의 속성, URL에 대한 정보 담고있음
+  var id = location.hash.substring(7);
+
+  // 상세 뉴스 API에 GET 요청
+  // JSON 데이터 파싱 
+  var newsContent = getData(CONTENT_URL.replace("@id", id));
+  container.innerHTML = "\n        <h1>".concat(newsContent.title, "</h1>\n        <div>\n            <a href=\"#/page/").concat(store.currentPage, "\">\uBAA9\uB85D\uC73C\uB85C</a>\n        </div>\n    ");
+}
+
+// 라우터: 뉴스 피드와 뉴스 내용 화면 중계
+function router() {
+  // 해시 값을 기준으로 화면 전환
+  var routePath = location.hash;
+
+  // location.hash에 #만 들어있을 경우 빈 값 반환
+  if (routePath === '') {
+    // 도입 화면
+    newsFeed();
+  } else if (routePath.indexOf('#/page/') >= 0) {
+    // 1️⃣ 이전, 이후 페이지 이동
+    // 2️⃣ 뉴스 내용 -> 뉴스 피드
+    store.currentPage = Number(routePath.substring(7));
+    newsFeed();
+  } else {
+    // 뉴스 피드 -> 뉴스 내용 
+    newsDetail();
+  }
+}
 
 // 뉴스 항목 클릭 시 상세 내용 표시 (hashChange 이벤트)
 // 해시 값: URL의 # 뒤에 오는 부분으로, 페이지 내 특정 위치나 컨텐츠를 식별하는데 사용
-window.addEventListener('hashchange', function () {
-  // location: window 객체의 속성, URL에 대한 정보 담고있음
-  // #을 제외한 나머지 부분 id 가져옴
-  var id = location.hash.substring(1);
+window.addEventListener('hashchange', router);
 
-  // 상세 뉴스 API에 GET 요청
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-  ajax.send();
-
-  // JSON 데이터 파싱 
-  var newsContent = JSON.parse(ajax.response);
-  var title = document.createElement('h1');
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
-
-// 뉴스 목록 표시 
-for (var i = 0; i < 10; i++) {
-  var li = document.createElement('li');
-  var a = document.createElement('a');
-  a.href = "#".concat(newsFeed[i].id);
-  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
-  li.appendChild(a);
-  ul.appendChild(li);
-}
-container.appendChild(ul);
-container.appendChild(content);
+// 첫 진입 화면
+router();
 },{}],"../../../Users/preen/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -196,7 +218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54774" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64639" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
